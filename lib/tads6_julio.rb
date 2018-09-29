3eb2302542db02fa55ea1ce51894c796a96fff9e
loardjulio <juliotorreforte@gmail.com>
2018-09-28 21:03:05 -0400

    Commit inicial

---
+++ lib/tads6_julio.rb
@@ -0,0 +1,5 @@
+require "tads6_julio/version"
+
+module Tads6Julio
+  # Your code goes here...
+end
---
+++ spec/tads6_julio_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Tads6Julio do
+  it "has a version number" do
+    expect(Tads6Julio::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
